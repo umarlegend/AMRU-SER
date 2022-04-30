c9687d8a8fad65135c881c9c52e7914cf9d3e376
umarlegend <103824676+umarlegend@users.noreply.github.com>
2022-05-01 02:57:31 +0500

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *AMRU-SER*:  ```" + msg + "```"
+    return "✅ *UMAR-DEV*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *AMRU-SER*:  ```" + msg + "```"
+    return "🛑 *UMAR-DEV*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *AMRU-SER*:  ```" + msg + "```"
+    return "⏺️ *UMAR-DEV*:  ```" + msg + "```"
 }
 
 
