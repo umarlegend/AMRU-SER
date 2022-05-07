ab317dcde48b333fdee9e42c8c0369e26a360281
umarlegend <103824676+umarlegend@users.noreply.github.com>
2022-05-07 22:09:23 +0500

    Update abu.js

--- abu.js
+++ abu.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+917025631103' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amru-ser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+923165123719' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Umar-dec' : process.env.DEPLOYER,    
 };
